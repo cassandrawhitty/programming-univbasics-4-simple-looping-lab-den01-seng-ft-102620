e7cac07dd91c25da3c2508fddac528d430bd4475
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-13 03:35:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
-# Write your methods here
+def loop_message_five_times(message) 
+  message = "Hello World!"
+  count = 0
+  while count < 5 
+  do
+    puts message
+    count += 1
+  end
+end
+
+def loop_message_n_times(message, limit)
+  message = "Hello Moon!"
+  count = 0
+  while count < limit
+  do
+    puts message
+    count += 1
+  end
+end
+
+def output_array
+end
+
+def return_string_array
+end
