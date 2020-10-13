82b93c81d106a3a0b860b4f2b4859934a062e782
Cassandra Whitty <cassandraallexwhitty@gmail.com>
2020-10-13 03:37:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,7 +8,7 @@ def loop_message_five_times(message)
 end
 
 def loop_message_n_times(message, limit)
-  message = "Hello Moon!"
+  message = "Hello Red Balloon."
   count = 0
   while count < limit
     puts message
